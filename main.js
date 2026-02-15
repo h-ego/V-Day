7d9ed3720d57564779e2f645112885bead4df33d
h-ego <111367716+h-ego@users.noreply.github.com>
2026-02-15 11:23:49 +0000

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,3 @@ onload = () => {
   document.body.classList.remove("container");
 };
 
-  // Add this to the very top of flower.html
-  if (performance.navigation.type === 1) {
-    // If the page is reloaded, go back to the question card
-    window.location.href = "index.html";
-  }
